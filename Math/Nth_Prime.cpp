7006eaa81b41d2beccefbd0ddf429efe0751a7eb
Josue Huaroto Villavicencio <46503611+HeNeos@users.noreply.github.com>
2020-10-29 23:56:52 -0500

    Updating the tabs :p

--- Math/Nth_Prime.cpp
+++ Math/Nth_Prime.cpp
@@ -24,16 +24,16 @@ pair <long,long> limits(ll n){
 }
 
 vector <int> prime_sieve(){
-    vector <bool> sieve(limit,false);
+	vector <bool> sieve(limit,false);
 	vector <int> ans;
-    for(int i=2; i<limit; i++){
-        if(!sieve[i]){
+	for(int i=2; i<limit; i++){
+		if(!sieve[i]){
 			pi_sieve[i] = pi_sieve[i-1] + 1;
-            ans.push_back(i);
-            for(ll j=1LL*i*i; j<limit; j+=i) sieve[j] = 1;
-        }
+			ans.push_back(i);
+			for(ll j=1LL*i*i; j<limit; j+=i) sieve[j] = 1;
+		}
 		else pi_sieve[i] = pi_sieve[i-1];
-    }
+	}
 	return ans;
 }
 
@@ -42,12 +42,12 @@ vector <int> primes;
 map <pair <ll,ll>, ll> phi_cache;
 
 ll phi(ll x, ll a){
-    pair <ll,ll> values = {x,a};
-    if(phi_cache.find(values) != phi_cache.end()) return phi_cache[values];      
-    if(a == 1) return (x+1)/2;
-    ll ans = phi(x, a-1) - phi(x/primes[a-1], a-1);
-    phi_cache[values] = ans;
-    return ans;
+	pair <ll,ll> values = {x,a};
+	if(phi_cache.find(values) != phi_cache.end()) return phi_cache[values];      
+	if(a == 1) return (x+1)/2;
+	ll ans = phi(x, a-1) - phi(x/primes[a-1], a-1);
+	phi_cache[values] = ans;
+	return ans;
 }
 
 map <ll,ll> pi_cache;
@@ -57,39 +57,39 @@ ll pi(ll x){
 
 	if(pi_cache.find(x) != pi_cache.end()) return pi_cache[x];
 
-    int a = pi((int)(pow(x,1.0/4)));
-    int b = pi((int)(sqrt(x)));
-    int c = pi((int)(pow(x,1.0/3)));
+	int a = pi((int)(pow(x,1.0/4)));
+	int b = pi((int)(sqrt(x)));
+	int c = pi((int)(pow(x,1.0/3)));
 
-    ll ans = phi(x,a) + 1LL*(b+a-2)*(b-a+1)/2;
+	ll ans = phi(x,a) + 1LL*(b+a-2)*(b-a+1)/2;
 
-    for(int i=a+1; i<b+1; i++){
-        ll w = x/primes[i-1];
-        ans -= pi(w);
-        if(i <= c){
-            ll b_i = pi((int)(sqrt(w)));
+	for(int i=a+1; i<b+1; i++){
+		ll w = x/primes[i-1];
+		ans -= pi(w);
+		if(i <= c){
+			ll b_i = pi((int)(sqrt(w)));
 			for(int j=i; j<b_i+1; j++)
-                ans = ans - pi(w/primes[j-1]) + j - 1;
-        }
-    }
-    pi_cache[x] = ans;
-    return ans;
+				ans = ans - pi(w/primes[j-1]) + j - 1;
+		}
+	}
+	pi_cache[x] = ans;
+	return ans;
 }
 
 int main(){
 	ll n;
 	cin >> n;
 	
-  pair <ll, ll> LIMITS = limits(n);
+	pair <ll, ll> LIMITS = limits(n);
 	N = LIMITS.second;
 	limit = min((int)(20*sqrt(2*N)),10000000); //optional: use fixed 1e7 and use bitset in prime_sieve :)
 	ll lo = LIMITS.first;
 	ll hi = LIMITS.second;
   
-  pi_sieve.resize(limit);
-  primes = prime_sieve();
+	pi_sieve.resize(limit);
+	primes = prime_sieve();
   
-  short int flag = 0;
+	short int flag = 0;
 	while(lo < hi){
 		ll me = lo + (hi-lo)/2;
 		ll test = pi(me); //optional: comment next if statement, it's a poorly optimization :(
@@ -108,5 +108,5 @@ int main(){
 		else lo = me + 1;
 	}
 	cout << lo << '\n';
-  return 0;
+	return 0;
 }
