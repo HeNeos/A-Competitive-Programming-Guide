9802f458293df7094289147dd25b33e5c9fd5e78
Josue Huaroto Villavicencio <46503611+HeNeos@users.noreply.github.com>
2021-03-13 23:26:25 -0500

    Update suffix_automaton.cpp

--- Strings/suffix_automaton.cpp
+++ Strings/suffix_automaton.cpp
@@ -6,7 +6,6 @@ struct SuffixAutomaton {
 	//len[i] : the length of the longest string in the ith class
 	//edges[i]  : the labeled edges from node i
 	vector<vi> iLink; // inverse links
-	vector<int> terminals;
 	int add(int p, char c){
 		auto getEdge = [&]() {
 			if (p == -1) return 0;
@@ -36,45 +35,53 @@ struct SuffixAutomaton {
 
 
 /*
-	// APPLICATIONS
-
-  // Suffix Array:
-  SuffixAutomaton S; string s; vi sa;
-  void SuffixArray(int x = 0){
-    if(!S.isClone[x]) sa.pb(s.size()-1-S.pos[x]);
-    vector<pair<char,int>> chr;
-    for(int i=0; i<S.iLink[x].size(); i++){
-      chr.pb({s[S.pos[S.iLink[x][i]]-S.len[x]], S.iLink[x][i]});
-    }
-    sort(chr.begin(), chr.end());
-    for(int i=0; i<chr.size(); i++) SuffixArray(chr[i].second);
-  }
-  
-  void getAllOccur(vi& oc, int v) {
+// APPLICATIONS
+// Suffix Array:
+	SuffixAutomaton S;
+	string s;
+	vi sa;
+	void SuffixArray(int x = 0){
+		if(!S.isClone[x]) sa.pb(s.size()-1-S.pos[x]);
+		vector<pair<char,int>> chr;
+		for(int i=0; i<S.iLink[x].size(); i++){
+			chr.pb({s[S.pos[S.iLink[x][i]]-S.len[x]], S.iLink[x][i]});
+		}
+		sort(chr.begin(), chr.end());
+		for(int i=0; i<chr.size(); i++) SuffixArray(chr[i].second);
+	}
+  	
+//String Matching
+	void getAllOccur(vi& oc, int v) {
 		if (!isClone[v]) oc.pb(pos[v]); // terminal position
-		each(u,iLnk[v]) getAllOccur(oc,u);
+		for(int i=0; i<iLink[v].size(); i++) getAllOccur(oc, iLink[v][i]);
 	}
-	vi allOccur(str s) { // get all occurrences of s in automaton
+	vi allOccur(string s) { // get all occurrences of s in automaton
 		int cur = 0;
-		each(x,s) {
-			if (!edges[cur].count(x)) return {};
-			cur = edges[cur][x]; }
-		vi oc; getAllOccur(oc,cur); each(t,oc) t += 1-sz(s); // convert end pos -> start pos
-		sort(all(oc)); return oc;
+		for(int i=0; i<s.size(); i++){
+			if (!edges[cur].count(s[i])) return {};
+			cur = edges[cur][s[i]];
+		}
+		vi oc; getAllOccur(oc,cur); 
+		for(int i=0; i<oc.size(); i++) oc[i] += 1-s.size(); // convert end pos -> start pos
+		sort(oc.begin(), oc.end()); return oc;
 	}
-	vl distinct;
+	
+//Substring count
+	vector <ll> distinct;
 	
 	ll getDistinct(int x) { // # of distinct strings starting at state x
 		if (distinct[x]) return distinct[x];
-		distinct[x] = 1; each(y,edges[x]) distinct[x] += getDistinct(y.s);
+		distinct[x] = 1; 
+		for(auto v:edges[x]) distinct[x] += getDistinct(v.second);
 		return distinct[x];
 	}
 	ll numDistinct() { // # distinct substrings including empty
-		distinct.rsz(N); return getDistinct(0);
+		distinct.resize(N); return getDistinct(0);
 	}
 	
-	ll numDistinct2() { // assert(numDistinct() == numDistinct2());
-		ll ans = 1; FOR(i,1,N) ans += len[i]-len[link[i]];
+	ll countSubStrings{
+		ll ans = 1; 
+		for(int i=1; i<N; i++) ans += len[i]-len[link[i]];
 		return ans;
-	} 
+	}
 */
