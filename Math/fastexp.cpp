31e333ff72f35adaf34e383449aa1b950fa8723b
Josue Huaroto Villavicencio <46503611+HeNeos@users.noreply.github.com>
2021-07-10 10:30:24 -0500

    Create fastexp.cpp

---
+++ Math/fastexp.cpp
@@ -0,0 +1,13 @@
+ll fastexp(ll x, ll y, ll p){
+	ll ans = 1;
+	while(y > 0){
+		if(y&1) ans = (ans*x)%p;
+		y = y>>1;
+		x = (x*x)%p;
+	}
+	return ans%p;
+}
+
+ll inv(ll x, ll p){
+	return fastexp(x, p-2, p);
+}
