df4d0844542828b701731b2d1a4017d86162c130
Josue Huaroto Villavicencio <46503611+HeNeos@users.noreply.github.com>
2022-05-29 23:03:39 -0500

    Create ExtendedEuclidesAlgorithm.cpp

---
+++ ExtendedEuclidesAlgorithm.cpp
@@ -0,0 +1,12 @@
+ll extended_gcd(ll a, ll b, ll& x, ll& y){ // a*x + b*y == gcd(a,b)
+    if (b == 0){
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    ll d = extended_gcd(b, a%b, x1, y1);
+    x = y1;
+    y = x1 - y1*(a/b);
+    return d;
+}
