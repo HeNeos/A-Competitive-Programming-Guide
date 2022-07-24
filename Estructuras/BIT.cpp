adad799c6c3b7fddc5ffb76476a8f32092e36493
Josue Huaroto Villavicencio <46503611+HeNeos@users.noreply.github.com>
2022-07-24 01:11:06 -0500

    Create BIT.cpp

---
+++ Estructuras/BIT.cpp
@@ -0,0 +1,81 @@
+int BIT[N];
+
+// Point Update - Range Query
+
+void init(vector <int> a){
+    BIT[0] = a[0];
+    for(int i=1; i<a.size(); i++)
+        BIT[i] = BIT[i-1] + a[i];
+
+    for(int i=a.size()-1; i>0; i--){
+        int low = (i&(i+1)) - 1;
+        if(low >= 0) BIT[i] -= BIT[low];
+    }
+}
+
+void update(int pos, int v){
+    for(++pos; pos<N; pos += (pos&-pos))
+        BIT[pos] += v;
+}
+
+int query(int r){
+    int ans = 0;
+    for(++pos; r>0; r -= (r&-r))
+        ans += BIT[r];
+    return ans;
+}
+
+int query(int l, int r){
+    return query(r) - query(l-1);
+}
+
+// Range Update - Point Query
+
+void update(int pos, int v) {
+    for (++pos; pos<N; pos+=(idx&-idx))
+        BIT[pos] += v;
+}
+
+void range_update(int l, int r, int v) {
+    add(l, v);
+    add(r+1, -v);
+}
+
+int query(int pos) {
+    int ans = 0;
+    for (++pos; pos>0; idx-=(idx&-idx))
+        ans += BIT[pos];
+    return ans;
+}
+
+// Range Update - Range Query
+
+vector <int> B1(N, 0);
+vector <int> B2(N, 0);
+
+void update(vector <int> &b, int pos, int v){
+    for(++pos; pos<N; pos+=(pos&-pos))
+        b[pos] += v;
+}
+
+void range_update(int l, int r, int v){
+    update(B1, l, v);
+    update(B1, r+1, -v);
+    add(B2, l, v*(l-1));
+    add(B2, r+1, -v*r);
+}
+
+int query(vector <int> &b, int pos){
+    int ans = 0;
+    for(++pos; pos>0; pos-=(pos&-pos))
+        ans += b[pos];
+    return ans;
+}
+
+int point_query(int pos){
+    return query(B1, pos)*pos -  query(B2, pos);
+}
+
+int range_query(int l, int r){
+    return point_query(r) - point_query(l-1);
+}
